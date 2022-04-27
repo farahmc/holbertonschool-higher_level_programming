d8206ef728f28eb81c42dea8f941a12bb970fd6e
Farah <farah.mccurdy@gmail.com>
2022-04-26 17:56:54 -0700

    fixed condition for negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ number_string = repr(number)
 last_number_string = number_string[-1]
 last_number = int(last_number_string)
 
-if last_number > 5:
+if number > 0 and last_number > 5:
     print(f"Last digit of {number} is {last_number} and is greater than 5")
-elif number < 0 and last_number < 6 and last_number != 0:
+elif number < 0 and last_number != 0:
     print(f"Last digit of {number} is -{last_number} and is less than 6 and not 0")
 elif number > 0 and last_number < 6 and last_number != 0:
     print(f"Last digit of {number} is {last_number} and is less than 6 and not 0")
