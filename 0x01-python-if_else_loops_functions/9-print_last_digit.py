82b63c08b4001590eaef34b97b0d09dc49038c2a
Farah <farah.mccurdy@gmail.com>
2022-04-26 19:47:38 -0700

    added file

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    number_string = repr(number)
+    last_number_string = number_string[-1]
+    last_number = int(last_number_string)
+    print(f"{last_number}", end="")
+
+    return(last_number)
