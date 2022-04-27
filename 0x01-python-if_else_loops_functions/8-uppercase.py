a29c8e853ee68441df9071f4e657b528da253c27
Farah <farah.mccurdy@gmail.com>
2022-04-26 21:38:45 -0700

    fixed variable name

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,10 @@
 
 
 def uppercase(str):
-
     for i in str:
         if i >= 'a' and i <= 'z':
             conv_to_int = ord(i) - 32
-            conv_to_upper = chr(conv_to_int)
+            i = chr(conv_to_int)
         print(i, end="")
 
     print("")
