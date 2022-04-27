53eb1f26636b4de8dbfcb8ae213cc0a55c9bf3f3
Farah <farah.mccurdy@gmail.com>
2022-04-26 19:13:31 -0700

    fixed pep8 and removed trailing space following last digit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 100):
     if i != 99:
-        print(f"{i:02}", end = ", ")
+        print(f"{i:02}", end=", ")
     else:
-        print(f"{i:02}", end = " \n")
+        print(f"{i:02}", end="\n")
