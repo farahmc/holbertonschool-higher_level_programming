bc582df7bc7cfc8c9fc3c082d08d6e77a67c0331
Farah <farah.mccurdy@gmail.com>
2022-04-26 17:53:00 -0700

    added condition for negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,9 @@ last_number = int(last_number_string)
 
 if last_number > 5:
     print(f"Last digit of {number} is {last_number} and is greater than 5")
-elif last_number < 6 and last_number != 0:
+elif number < 0 and last_number < 6 and last_number != 0:
+    print(f"Last digit of {number} is -{last_number} and is less than 6 and not 0")
+elif number > 0 and last_number < 6 and last_number != 0:
     print(f"Last digit of {number} is {last_number} and is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is {last_number} and is 0")
