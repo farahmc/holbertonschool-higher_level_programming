3ebb618435a43019968e9ce0106e66d5b3fa8279
Farah <farah.mccurdy@gmail.com>
2022-05-01 21:50:01 -0700

    fix: change first character of sentence if sentence is null

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0]
     if sentence is None:
-        first_char = None
+        sentence[0] = None
     return(length, first_char)
