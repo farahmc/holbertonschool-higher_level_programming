933ce812283c54247e13b04f9ca1a2cacab8cce7
Farah <farah.mccurdy@gmail.com>
2022-05-01 22:00:01 -0700

    change: if condition to if sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0]
-    if sentence is not None:
-        return(length, first_char)
-    else:
+    if sentence == "":
         return(length, None)
+
+    return(length, first_char)
