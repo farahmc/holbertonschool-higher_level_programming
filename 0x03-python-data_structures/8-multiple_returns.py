8dc6fe7cc4d3b438b754081516267d38db92ec6c
Farah <farah.mccurdy@gmail.com>
2022-05-01 22:23:16 -0700

    fix: removed variable for first char

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
-    if sentence == "":
+    if sentence == '':
         return(0, None)
-    return(length, first_char)
+    return(length, sentence[0])
