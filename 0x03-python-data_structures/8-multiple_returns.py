c222909236b14c8101790768c186dbddd18aa8e0
Farah <farah.mccurdy@gmail.com>
2022-05-01 21:52:47 -0700

    add: return for if sentence is null

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0]
     if sentence is None:
-        sentence[0] = None
+        return(0, None)
     return(length, first_char)
