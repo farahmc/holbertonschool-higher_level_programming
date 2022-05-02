fc7b7ec52ed7a8f83bfd075f25b5e332658a105a
Farah <farah.mccurdy@gmail.com>
2022-05-01 21:54:50 -0700

    changed: reversed condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0]
-    if sentence is None:
-        return(0, None)
-    return(length, first_char)
+    if sentence is not None:
+        return(length, first_char)
+    else:
+        return(length, None)
