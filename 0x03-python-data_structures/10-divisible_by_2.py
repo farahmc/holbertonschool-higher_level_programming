05bcdd31ff070925b3beab6ef35c4ece934b5136
Farah <farah.mccurdy@gmail.com>
2022-05-01 22:58:29 -0700

    change: instead of changing value using index, used append

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,10 +3,10 @@ def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
         return(None)
 
-    copy_list = list(my_list)
-    for i in copy_list:
+    copy_list = []
+    for i in my_list:
         if i % 2 == 0:
-            copy_list[i] = True
+            copy_list.append(True)
         else:
-            copy_list[i] = False
+            copy_list.append(False)
     return(copy_list)
