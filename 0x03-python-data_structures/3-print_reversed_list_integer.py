8ced5c241c2506c12edb9d71bb2d7bf8ac4b98ff
Farah <farah.mccurdy@gmail.com>
2022-05-01 19:38:57 -0700

    fix: cast output to int, remove: return

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is not None:
         rev_list = reversed(my_list)
         for i in rev_list:
-            print(i)
-
-    return(my_list)
+            print(int(i))
