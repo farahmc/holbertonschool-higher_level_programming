6948a37969eee81452aa0bca61547bbe77d2a0e3
Farah <farah.mccurdy@gmail.com>
2022-05-01 22:07:55 -0700

    change: is to == for if sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0]
-    if sentence is "":
+    if sentence == "":
         return(0, None)
     return(length, first_char)
