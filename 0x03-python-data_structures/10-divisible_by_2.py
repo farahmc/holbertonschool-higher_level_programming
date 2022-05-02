bdf8e8313419b1aec3aed8e0136c6d2633b6b2cb
Farah <farah.mccurdy@gmail.com>
2022-05-01 22:51:57 -0700

    add: condition for if list is empty

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return(None)
+
     copy_list = list(my_list)
     for i in copy_list:
         if i % 2 == 0:
