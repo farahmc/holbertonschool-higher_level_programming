30305053491ae0e698e1d740f97def36d1cd2d69
Farah <farah.mccurdy@gmail.com>
2022-05-03 20:27:08 -0700

    added file

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/bin/bash/python3
+def search_replace(my_list, search, replace):
+    copy_list = []
+    for i in my_list:
+        if search == i:
+            copy_list.append(replace)
+        else:
+            copy_list.append(i)
+    return copy_list
