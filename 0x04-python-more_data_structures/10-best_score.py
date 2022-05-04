2091072f98a26733fd87cb085c3c237c1c7b8f10
Farah <farah.mccurdy@gmail.com>
2022-05-04 00:14:13 -0700

    change: early return first, added check for if not dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        return(sorted(a_dictionary, key=a_dictionary.get)[-1])
-    return None
+    if a_dictionary is None or not a_dictionary:
+        return None
+    return(sorted(a_dictionary, key=a_dictionary.get)[-1])
