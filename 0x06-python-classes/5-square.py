b9d9825577c4be5510a8502c00d39fda1627db3d
Farah <farah.mccurdy@gmail.com>
2022-05-16 21:24:48 -0700

    add: file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Square:
+    """A class Square with private instance attribute size
+    Instantiation with size
+    raise errors if size is not an int or less than 0
+    Public instance method area which returns the square area"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size**2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
