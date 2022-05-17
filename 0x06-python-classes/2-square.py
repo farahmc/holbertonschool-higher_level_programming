6c2d0b7ad3a28e1b2a843366c8b4c2e29b01bb3f
Farah <farah.mccurdy@gmail.com>
2022-05-16 22:45:10 -0700

    add: more docs

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,12 @@
 
 
 class Square:
+    """A class Square"""
     def __init__(self, size=0):
+        """Raise errors if size is not an int or less than 0"""
         if isinstance(size, int) is not True:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        """Instantiate with size"""
         self.__size = size
