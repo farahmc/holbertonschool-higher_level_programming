7044cf9fc728e4d14d262a5971a052aace803bec
Farah <farah.mccurdy@gmail.com>
2022-05-16 19:47:42 -0700

    add: file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """A class Square with private instance attribute size
+    Instantiation with size"""
+
+    def __init__(self, size):
+        self.__size = size
