9150e660c97b61a4c4de66ecb0d0e5cef9db2320
Farah <farah.mccurdy@gmail.com>
2022-05-17 05:21:01 -0700

    add: method __str__

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -12,10 +12,12 @@
 class Square:
     """
     A class Square
-
-    Instantiation with size
     """
+
     def __init__(self, size=0, position=(0, 0)):
+        """
+        Instantiation with size
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -32,36 +34,36 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
-    """
-    Get the size of the square
-    """
     @property
     def size(self):
+        """
+        Get the size of the square
+        """
         return self.__size
 
-    """
-    Set the size of the square
-    """
     @size.setter
     def size(self, value):
+        """
+        Set the size of the square
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    """
-    Get the position of the square
-    """
     @property
     def position(self):
+        """
+        Get the position of the square
+        """
         return self.__position
 
-    """
-    Set the position of the square
-    """
     @position.setter
     def position(self, value):
+        """
+        Set the position of the square
+        """
         if (
                 not isinstance(self.__position, tuple)
                 or len(value) != 2
@@ -72,16 +74,16 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-    """
-    Returns the square area
-    """
     def area(self):
+        """
+        Returns the square area
+        """
         return (self.__size**2)
 
-    """
-    Prints a square
-    """
     def my_print(self):
+        """
+        Prints a square
+        """
         if self.__size == 0:
             print()
         else:
@@ -93,3 +95,21 @@ class Square:
                 for col in range(self.__size):
                     print("#", end="")
                 print()
+
+    def __str__(self):
+        """
+        a Square instance should have the same behaviour as my_print
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__position[1]):
+                print()
+            for row in range(self.__size):
+                for col in range(self.__position[0]):
+                    print(" ", end="")
+                for col in range(self.__size):
+                    print("#", end="")
+                if row != self.__size - 1:
+                    print()
+        return ("")
