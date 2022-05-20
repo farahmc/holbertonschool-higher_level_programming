cef99317c5cd564930a1699fcf77a721c58762c5
Farah <farah.mccurdy@gmail.com>
2022-05-19 21:57:51 -0700

    add: logic for edge cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,19 +15,22 @@ def matrix_divided(matrix, div):
     Returns:
     New matrix with quotients to 2 decimal places
     """
-    """if (
-        not all(isinstance(element, int) for element in matrix)
-        and not all(isinstance(element, float) for element in matrix)
-    ):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")"""
-
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
     if div == 0 or div == -0:
         raise ZeroDivisionError("division by zero")
 
-    """if not all(len(row) == len(matrix[0]) for row in matrix:
-        raise TypeError("Each row of the matrix must have the same size")"""
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    return [list(map(lambda element: round(element/div, 2), row)) for row in  matrix]
+    length = len(matrix[0])
+    print(length)
+    for row in matrix:
+        print(f"length of row elements: {len(row)}")
+        if len(row) != length:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    new_list = [list(map(lambda element: round(element/div, 2), row)) for row in  matrix] 
+    return new_list
