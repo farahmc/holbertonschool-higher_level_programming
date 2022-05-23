0eb303107f6b242949162e2947f25a7c497408e4
Farah <farah.mccurdy@gmail.com>
2022-05-22 21:30:52 -0700

    add: docs for __del__

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -78,7 +78,7 @@ class Rectangle:
 
     def __str__(self):
         """
-        Return a string representation
+        Return a printable representation
         """
         string_rep = ""
         if self.width == 0 or self.height == 0:
@@ -91,8 +91,14 @@ class Rectangle:
         return string_rep
 
     def __repr__(self):
+        """
+        Return a string representation
+        """
         return f'Rectangle({self.width},{self.height})'
 
     def __del__(self):
+        """
+        Print a message every time Rectangle is deleted
+        """
         print('Bye rectangle...')
         type(self).number_of_instances -= 1
