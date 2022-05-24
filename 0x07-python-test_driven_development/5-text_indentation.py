f83c656b98d17294c91691b338bc17d711e6e251
Farah <farah.mccurdy@gmail.com>
2022-05-23 22:08:49 -0700

    fix: pep8, add: whitespace

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,9 +8,11 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     if len(text) == 0:
         raise ValueError("string cannot be empty")
-    text = text.translate({ord('.'):'.\n', ord('?'):'?\n', ord(':'):':\n'})
+
+    text = text.translate({ord('.'): '.\n', ord('?'): '?\n', ord(':'): ':\n'})
     new = ''.join([line.strip()+'\n\n' for line in text.splitlines()])
     new = new.rstrip('\n')
+
     if new[-1] == "?" or new[-1] == "." or new[-1] == ":":
         new += "\n\n"
     print(new, end="")
