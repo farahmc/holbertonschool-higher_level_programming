7ff870ca87ff0981974653fc554d1d33cf72901d
Farah <farah.mccurdy@gmail.com>
2022-05-29 20:34:17 -0700

    add: description for module

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""
+Defines a class called MyList
+"""
+
 
 class MyList(list):
     """
     Class that inherits from list
+
+    Attributes:
+    print_sorted - prints sorted list
     """
 
     def print_sorted(self):
