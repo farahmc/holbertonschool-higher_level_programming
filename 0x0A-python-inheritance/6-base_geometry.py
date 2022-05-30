9a3ef277c9f852b919d6b61a24f7d7f1c9e6bf03
Farah <farah.mccurdy@gmail.com>
2022-05-29 21:10:16 -0700

    add: file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A class that raises an exception
+"""
+
+
+class BaseGeometry:
+    """
+    A class that raises an exception
+    """
+    def area(self):
+        """
+        Raise an exception
+        """
+        raise Exception("area() is not implemented")
