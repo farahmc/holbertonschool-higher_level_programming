3cb74c64519c116c152c8e5143a724927cbea0ef
Farah <farah.mccurdy@gmail.com>
2022-05-30 04:11:34 -0700

    add: check if name is a string

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -27,3 +27,5 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+        if type(name) != str:
+            raise TypeError(f"{name} must be a string")
