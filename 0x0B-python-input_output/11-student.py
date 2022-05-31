69f0cdbc22054d61706a163cec8bf7488ddaede3
Farah <farah.mccurdy@gmail.com>
2022-05-31 04:00:09 -0700

    add: file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Class student that defines a student
+"""
+
+
+class Student:
+    """
+    Define a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiate class
+
+        Arguments:
+        first_name - first name of student
+        last_name - last name of student
+        age - age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance
+        If attrs is a list of strings, only attribute names contained in this
+        list must be retrieved
+        """
+        if type(attrs) == list and all(type(el) == str for el in attrs):
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replace all attributes of the student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
