280f45ef8a1ad85d9f53c64ca4b2afb7765d7e24
Farah <farah.mccurdy@gmail.com>
2022-06-03 00:15:32 -0700

    update: task 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,9 +2,10 @@
 """
 A class to display a representation of a rectangle
 """
+from models.base import Base
 
 
-class Rectangle:
+class Rectangle(Base):
     """
     Defines a representation of a rectangle
     """
@@ -18,6 +19,7 @@ class Rectangle:
         x - x
         y - y
         """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
@@ -25,10 +27,40 @@ class Rectangle:
 
     @property
     def width(self):
+        """ Get the width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ Set the width """
         self.__width = value
 
-    
+    @property
+    def height(self):
+        """ Get the height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Set the height """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Get x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Set x """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Get y """
+        return self.__y
+
+    @y.setter
+        """ Set y """
+    def y(self, value):
+        self.__y = value
