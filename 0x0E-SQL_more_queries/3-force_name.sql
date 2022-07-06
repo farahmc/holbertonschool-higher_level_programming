a4cbd6a1b9351114a9d7eb1233b3b9547aef296a
Farah <farah.mccurdy@gmail.com>
2022-07-05 19:38:41 -0700

    add: extra comments as per requirements

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,7 @@
 -- script that creates the table force_name
+-- create the table, no error if table exists
 CREATE TABLE IF NOT EXISTS force_name (
+-- column, datatype, if value should be null or not null
        id INT,
        name VARCHAR(256) NOT NULL
 );
