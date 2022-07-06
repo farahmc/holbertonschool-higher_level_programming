e5a6626d9274113275f0ddbbbc9a5c5990132fea
Farah <farah.mccurdy@gmail.com>
2022-07-05 20:27:29 -0700

    change: removed db name in FROM

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- script that lists all the cities of California found in the database hbtn_0d_usa
 -- list cities with state_id of California
 SELECT id, name
-       FROM hbtn_0d_usa.cities
+       FROM cities
        WHERE state_id = 1
        ORDER BY id;
