da9c7c68447feaa5d74ae4c1ed718752268a2642
Farah <farah.mccurdy@gmail.com>
2022-07-05 18:50:18 -0700

    add: file

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- script that creates the MySQL server user user_0d_1
+-- all privileges, set password. script should not fail if user exists
+CREATE USER
+       IF NOT EXISTS 'user_0d_1'@'localhost'
+       IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+      ON *.* TO 'user_0d_1'@'localhost'
+      WITH GRANT OPTION;
+FLUSH PRIVILEGES;
