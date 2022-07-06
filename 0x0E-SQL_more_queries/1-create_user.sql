7bd963f50789d31dc1a442c557beedafbe60a870
farahmc <86407866+farahmc@users.noreply.github.com>
2022-07-06 11:56:09 +1000

    change: formatting

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,7 +3,7 @@
 CREATE USER
        IF NOT EXISTS 'user_0d_1'@'localhost'
        IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-      ON *.* TO 'user_0d_1'@'localhost'
-      WITH GRANT OPTION;
+GRANT ALL PRIVILEGES on *.* 
+       TO 'user_0d_1'@'localhost'
+       WITH GRANT OPTION;
 FLUSH PRIVILEGES;
