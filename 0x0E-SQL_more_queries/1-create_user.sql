cf74e7ee74d6eb335710b039ee77736424603b51
farahmc <86407866+farahmc@users.noreply.github.com>
2022-07-06 11:59:23 +1000

    remove: with grant option

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,6 +4,5 @@ CREATE USER
        IF NOT EXISTS 'user_0d_1'@'localhost'
        IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES on *.* 
-       TO 'user_0d_1'@'localhost'
-       WITH GRANT OPTION;
+       TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
