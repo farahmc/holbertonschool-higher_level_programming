49bb170349c1452696f6c0a5ea9153533b2ae5cb
Farah <farah.mccurdy@gmail.com>
2022-07-05 19:08:17 -0700

    add: comments as per requirements

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,12 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2
--- SELECT privilege, set password, script should not fail if db or user exists
+-- create database, no error if db exists
 CREATE DATABASE
        IF NOT EXISTS hbtn_0d_2;
+-- create user with password, no error if user exists
 CREATE USER
        IF NOT EXISTS 'user_0d_2'@'localhost'
        IDENTIFIED BY 'user_0d_2_pwd';
+-- grant SELECT privilege to user on this db
 GRANT SELECT on hbtn_0d_2.*
       TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
