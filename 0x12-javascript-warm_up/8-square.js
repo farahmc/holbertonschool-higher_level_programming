472beab6d7a4d1f8f172cae4be7881cdfe4dd413
Farah McCurdy <farah.mccurdy@gmail.com>
2022-07-31 21:23:18 -0700

    change: i inside for loop

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,11 +2,11 @@
 
 const arg = parseInt(process.argv[2], 10);
 const x = 'x';
-let i = 0;
 
 if (isNaN(arg)) {
   console.log('Missing size');
 } else {
+    let i = 0;
     for (i; i < arg; i++) {
 	console.log(x.repeat(arg));
     }
