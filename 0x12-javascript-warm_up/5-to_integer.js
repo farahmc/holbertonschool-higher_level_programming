c92dbe2f4e40b4b41bff6b1522ac2ded36f973d4
Farah McCurdy <farah.mccurdy@gmail.com>
2022-07-31 20:37:43 -0700

    remove: incorrect display

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const arg = parseInt(process.argv[2], 10);
 if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number is: ' + arg);
+  console.log('My number: ' + arg);
 }
