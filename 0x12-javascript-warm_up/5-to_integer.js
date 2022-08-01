c2ba9979a44da50220121c2cd5c0521371082f7b
Farah McCurdy <farah.mccurdy@gmail.com>
2022-07-31 20:34:45 -0700

    add: radix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arg = parseInt(process.argv[2]);
+const arg = parseInt(process.argv[2], 10);
 
 if (typeof arg === 'number' && isNaN(arg) !== true) {
   console.log('My number is: ' + arg);
