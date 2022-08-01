a83d31118b2aba6d001299425eb45d970c974f36
Farah McCurdy <farah.mccurdy@gmail.com>
2022-07-31 20:36:41 -0700

    changed back to original

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const arg = parseInt(process.argv[2], 10);
 
-if (typeof arg === 'number' && isNaN(arg) !== true) {
-  console.log('My number is: ' + arg);
-} else {
+if (isNaN(arg)) {
   console.log('Not a number');
+} else {
+  console.log('My number is: ' + arg);
 }
