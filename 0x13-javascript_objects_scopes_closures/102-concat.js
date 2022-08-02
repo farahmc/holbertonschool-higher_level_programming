2eed215d08d18581ec0cc60a4fcbee766c9756fc
Farah McCurdy <farah.mccurdy@gmail.com>
2022-08-01 23:13:22 -0700

    add: file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileA = fs.readFileSync(process.argv[2]);
+const fileB = fs.readFileSync(process.argv[3]);
+fs.writeFileSync(process.argv[4], fileA + fileB);
