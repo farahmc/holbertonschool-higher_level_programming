2351c0b7e30df79c62e7b83724ee6e63bdddc1c6
Farah <farah.mccurdy@gmail.com>
2022-08-03 04:37:03 -0700

    add: unique, autoincrement constraints, class State docs

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,11 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    class that inherits from Base
+    Attributes: id, name
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True
+                autoincrement=True)
     name = Column(String(128), nullable=False)
