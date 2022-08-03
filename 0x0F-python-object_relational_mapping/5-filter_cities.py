4c2d5c2da6fdd366102ac9ee95943fef0dc9281b
Farah <farah.mccurdy@gmail.com>
2022-08-02 23:34:18 -0700

    remove: unused variable

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,6 @@ import MySQLdb
 
 if __name__ == "__main__":
     match = sys.argv[4]
-    cities = ""
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
