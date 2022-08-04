94bb3c167b7550a380b6d435268fcfcf4d41193d
Farah <farah.mccurdy@gmail.com>
2022-08-03 18:44:59 -0700

    remove: autoincremet

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,6 +15,5 @@ class State(Base):
     Attributes: id, name
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True
-                autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
