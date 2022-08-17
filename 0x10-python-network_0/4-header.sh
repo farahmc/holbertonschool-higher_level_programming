36c3f34c7055f6c8074ec56f2fc806963aff1e04
Farah <farah.mccurdy@gmail.com>
2022-08-16 23:58:37 -0700

    fix: shellcheck

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL with header variable and value
-curl -s $1 -H "X-HolbertonSchool-User-Id: 98"
+curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
