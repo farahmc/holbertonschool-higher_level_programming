96459f92eb8abb27ddc1c852fb5e7fe4c88585aa
farahmc <86407866+farahmc@users.noreply.github.com>
2022-08-18 17:08:10 +1000

    add: response close

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,3 +11,4 @@ if __name__ == "__main__":
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
         print(f"\t- utf8 content: {html.decode('utf-8')}")
+        response.close()
