e3498ea13bf076bfdb43049e1c9d1c5f00127e20
Farah <farah.mccurdy@gmail.com>
2022-08-18 02:03:50 -0700

    add: file

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" using requests package - takes in a URL and an email, sends a POST request
+ to the passed URL with the email as a parameter, and displays the body of the
+ response (decoded in utf-8) """
+from requests import post
+from sys import argv
+
+
+data = {'email': argv[2]}
+r = post(argv[1], data)
+print(r)
