597e0640d1a4e07b9961f030a461d73d94d2c44c
Farah <farah.mccurdy@gmail.com>
2022-08-18 02:20:07 -0700

    change try except to if else

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,11 +2,11 @@
 """ script that fetches URL using package requests"""
 from requests import get
 from sys import argv
-from urllib.error import HTTPError
 
 
-try:
-    r = get(argv[1])
-    print("r.text")
-except HTTPError as error:
+r = get(argv[1])
+
+if r.status_code >= 400:
     print("Error code: {}".format(r.status_code))
+else:
+    print(r.text)
