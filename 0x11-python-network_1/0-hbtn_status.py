43d604ee3e8a2231bfc6c50c1c20c78033b7a86d
Farah <farah.mccurdy@gmail.com>
2022-08-18 17:31:19 -0700

    fix: tidied up

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
+from urllib.request import urlopen
 
 
 if __name__ == "__main__":
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body Response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- utf8 content: {}".format(html.decode()))
