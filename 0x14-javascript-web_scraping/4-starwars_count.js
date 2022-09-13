be56cf91de0f68f703b9b4c9a8ef7955b5ea736a
Farah <farah.mccurdy@gmail.com>
2022-09-12 22:33:00 -0700

    add: file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const url = process.argv[2];
+const wedge = 18;
+let count = -1;
+
+axios
+  .get(url)
+  .then(res => {
+    const dict = res.data.results;
+    for (const movies of dict) {
+      for (const character in movies.characters) {
+        if (character.includes(wedge)) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  })
+  .catch(error => {
+    console.error(error);
+  });
