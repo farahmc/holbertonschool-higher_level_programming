48e006baee611628acdd917480362fda11fd33a9
Farah <farah.mccurdy@gmail.com>
2022-09-12 22:57:39 -0700

    add: file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+!/usr/bin/node
+
+const axios = require('axios');
+const url = process.argv[2];
+const fs = require('fs');
+
+axios
+  .get(url)
+  .then(res => {
+    const content = res.data;
+    fs.writeFile(process.argv[3], content, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
